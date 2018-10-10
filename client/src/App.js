6642d80a95ec921b898c56a48315af12fcb64b22
jacobsilver2 <jacobsilver2@mac.com>
2018-10-10 16:15:07 -0400

    added working proxy.  refactored passport to use async/await

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,17 +6,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="/auth/google"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hi There
+            Sign in with google.
           </a>
         </header>
       </div>
