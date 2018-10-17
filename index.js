71f0da6de1d6041f34fe4b1fb5ac4fb93b0952e0
jacobsilver2 <jacobsilver2@mac.com>
2018-10-17 17:01:57 -0400

    added heroku post script to server

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+if (process.env.NODE_ENV === 'production') {
+    // making sure express will serve production assets.
+    app.use(express.static('client/build'));
+    // express will serve up the index.html if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
