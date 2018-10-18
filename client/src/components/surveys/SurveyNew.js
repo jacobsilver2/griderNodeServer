78d784b0f512484c3716f5539608e052b9c2a5d6
jacobsilver2 <jacobsilver2@mac.com>
2018-10-18 18:42:02 -0400

    added Redux Form and SurveyNew.js

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+    state = {  }
+    render() {
+        return (
+            
+        );
+    }
+}
+
+export default SurveyNew;
