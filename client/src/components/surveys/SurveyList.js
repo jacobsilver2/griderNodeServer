a0a75a81bf8d8f9358e717edc037dbb300baebe6
jacobsilver2 <jacobsilver2@mac.com>
2018-10-21 22:13:28 -0400

    making surveyList component

---
+++ client/src/components/surveys/SurveyList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions/index';
+
+class SurveyList extends Component {
+    state = {  }
+    render() {
+        return (
+            
+        );
+    }
+}
+
+export default connect()(SurveyList);
